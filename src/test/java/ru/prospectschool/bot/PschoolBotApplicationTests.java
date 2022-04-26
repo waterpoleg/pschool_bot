9cc12d2087344faca4f9ee5965a4cb1a33e5778f
Oleg Korneev <waterpoleg@gmail.com>
2022-04-26 23:50:59 +0300

    initial

---
+++ src/test/java/ru/prospectschool/bot/PschoolBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prospectschool.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PschoolBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
