ca9731981c9b55367ab82a5d244bf8f41f2522c6
Oleg Korneev <waterpoleg@gmail.com>
2022-05-11 23:42:52 +0300

    add response for commands

--- src/main/java/ru/prospectschool/bot/controller/IncomeMessageController.java
+++ src/main/java/ru/prospectschool/bot/controller/IncomeMessageController.java
@@ -22,18 +22,31 @@ public class IncomeMessageController {
                 responseMessage = "Идет набор в очные и онлайн группы!\n" +
                         "Спешите приобрести абонемент со СКИДКОЙ 30%!\n" +
                         "Всего 6400 руб за месяц  обучения с носителем языка!";
-            case (Commands.CONTACTS):
-            case (Commands.TEACHERS):
-            case (Commands.PRICES):
-            default: {
                 break;
-            }
+            case (Commands.CONTACTS):
+                responseMessage = "тел: +7 985 143 02 42\n" +
+                        "email: prospectschool@niime.ru\n" +
+                        "адрес: 124460, Москва, Зеленоград, ул. Академика Валиева, д.6, стр.6";
+                break;
+            case (Commands.TEACHERS):
+                break;
+            case (Commands.PRICES):
+                responseMessage = "Групповые занятия (1,5 часа):\n" +
+                        "8 занятий - 8000 руб\n" +
+                        "Мини-группы (1,5 часа):\n" +
+                        "8 занятий - 9200 руб.\n" +
+                        "Индивидуальные занятия (45 минут / 1 час)\n" +
+                        "Преподаватель носитель языка - 1600 руб./1800 руб.\n" +
+                        "Старший преподаватель носитель языка - 2000 руб./2200 руб.";
+                break;
+            default:
+                break;
         }
         /*switch (command) {
-            case (Commands.INFO.getCommand()):
-            case (Commands.CONTACTS):
-            case (Commands.TEACHERS):
-            case (Commands.PRICES):
+            case (CommandsEnum.INFO.getCommand()):
+            case (CommandsEnum.CONTACTS):
+            case (CommandsEnum.TEACHERS):
+            case (CommandsEnum.PRICES):
             default: {
                 break;
             }
